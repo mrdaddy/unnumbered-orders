47420e4de4553ab7abc852ec9b29931a90096cff
Andrei <mrdaddy@mail.ru>
2018-09-14 11:15:38 +0300

    develop

--- src/main/java/com/rw/unnumbered/orders/controllers/BaseController.java
+++ src/main/java/com/rw/unnumbered/orders/controllers/BaseController.java
@@ -3,6 +3,7 @@ package com.rw.unnumbered.orders.controllers;
 import com.rw.unnumbered.orders.dto.ErrorMessage;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.ConstraintViolationException;
+import java.net.ConnectException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,10 +40,52 @@ public class BaseController {
         List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
         if(e.getBindingResult().hasErrors()) {
             for(ObjectError oe: e.getBindingResult().getAllErrors()) {
-                ErrorMessage errorMessage = new ErrorMessage(ERROR_PREFIX.validation+"."+oe.getCodes()[0],oe.getDefaultMessage());
+                ErrorMessage errorMessage = new ErrorMessage(oe.getCodes()[0],oe.getDefaultMessage());
                 errors.add(errorMessage);
             }
         }
         return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
+    }
+
+ /*   @ExceptionHandler(BusinessSystemException.class)
+    public ResponseEntity<?> handleInvalidRequest(BusinessSystemException e, WebRequest request) {
+        List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+        ErrorMessage errorMessage = new ErrorMessage(ERROR_PREFIX.express+"."+e.getCode(),e.getMessage());
+        errors.add(errorMessage);
+        return new ResponseEntity(errors, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(XmlParserSystemException.class)
+    public ResponseEntity<?> handleInvalidRequest(XmlParserSystemException e, WebRequest request) {
+        List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+        ErrorMessage errorMessage;
+        HttpStatus status;
+        if("timeout".equals(e.getMessage())) {
+            status = HttpStatus.GATEWAY_TIMEOUT;
+            return new ResponseEntity(status);
+        } else {
+            status = HttpStatus.SERVICE_UNAVAILABLE;
+            return new ResponseEntity(errors, status);
+        }
+    }*/
+    @ExceptionHandler(ConnectException.class)
+    protected List<ErrorMessage> handleConnectException(ConnectException e) {
+        List<ErrorMessage> errors = new ArrayList<>();
+        errors.add(new ErrorMessage(ERROR_PREFIX.system+".database_error", e.getMessage()));
+        return errors;
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    protected List<ErrorMessage> handleDataAccessException(EmptyResultDataAccessException e) {
+        List<ErrorMessage> errors = new ArrayList<>();
+        errors.add(new ErrorMessage(ERROR_PREFIX.system+".database_error", e.getMessage()));
+        return errors;
+    }
+
+    @ExceptionHandler(SQLException.class)
+    protected List<ErrorMessage> handleSQLException(SQLException e) {
+        List<ErrorMessage> errors = new ArrayList<>();
+        errors.add(new ErrorMessage(ERROR_PREFIX.system+".database_error", e.getMessage()));
+        return errors;
     }
 }
