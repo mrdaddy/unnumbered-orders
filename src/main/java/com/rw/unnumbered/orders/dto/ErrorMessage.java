b47896eb8076006997b7a1ada26c4414e9bda5f7
Andrei <mrdaddy@mail.ru>
2018-09-14 15:48:17 +0300

    develop

--- src/main/java/com/rw/unnumbered/orders/dto/ErrorMessage.java
+++ src/main/java/com/rw/unnumbered/orders/dto/ErrorMessage.java
@@ -11,8 +11,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @ApiModel(description = "Информация об ошибке. В поле code первая часть - тип ошибки. Значения: validation - ошибки валидации входных значений, system - системные ошибки сервера СППД (например, соединение с БД или LDAP), express - ошибки или проблемы соединения со шлюзом XML или самой системы Экспресс, epd - ошибки или проблемы соединения с сервером ЭПД")
 public class ErrorMessage {
-    @ApiModelProperty(example = "validation.tripInformation.train", required = true, value = "Составной код ошибки. Пример NotNull - код ошибки.tripInformation - объект .train - поле (не обязательно при общей ошибке валидации)", dataType = "String")
+    @ApiModelProperty(example = "validation.NotNull.tripInformation.train", required = true, value = "Составной код ошибки. Пример NotNull - код ошибки.tripInformation - объект (только для ошибок валидации) .train - поле (только для ошибок валидации, не обязательно при общей ошибке валидации)", dataType = "String")
     private String code;
+
     @ApiModelProperty(example = "Не должно быть пустым", required = true, value = "Текст ошибки", dataType = "String")
     private String message;
 }
